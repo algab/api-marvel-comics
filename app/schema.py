dcb6db0402fbe0b1093451f7533061f42e65cadb
Álvaro Oliveira <alvarogab6@gmail.com>
2020-06-11 10:54:56 -0300

    update schema

--- app/schema.py
+++ app/schema.py
@@ -7,61 +7,156 @@ from app.types.event import Event
 from app.types.series import Series
 from app.types.storie import Storie
 
-from app.resolvers.character import list_characters, search_character
-from app.resolvers.comics import list_comics, search_comics
-from app.resolvers.creator import list_creators, search_creator
-from app.resolvers.event import list_events, search_event
-from app.resolvers.series import list_series, search_serie
-from app.resolvers.storie import list_stories, search_storie
+import app.resolvers.character as resolve_character 
+import app.resolvers.comics as resolve_comics
+import app.resolvers.creator as resolve_creator
+import app.resolvers.event as resolve_event
+import app.resolvers.series as resolve_series
+import app.resolvers.storie as resolve_storie
 
 class Query(ObjectType):
-    characters = List(Character, limit=Int(), offset=Int())
-    searchCharacter = Field(Character, id=ID(required=True))
-    comics = List(Comics, limit=Int(), offset=Int())
-    searchComics = Field(Comics, id=ID(required=True))
-    creators = List(Creator, limit=Int(), offset=Int())
-    searchCreator = Field(Creator, id=ID())
-    events = List(Event, limit=Int(), offset=Int())
-    searchEvent = Field(Event, id=ID())
-    series = List(Series, limit=Int(), offset=Int())
-    searchSerie = Field(Series, id=ID())
-    stories = List(Storie, limit=Int(), offset=Int())
-    searchStorie = Field(Storie, id=ID())
+    characters = List(Character, limit=Int(required=True), offset=Int())
+    searchCharacter = Field(Character, id=ID(required=True), resolver=resolve_creator.search_creator)
+    characterComics = List(Comics, id=ID(required=True), limit=Int(required=True), offset=Int())
+    characterEvents = List(Event, id=ID(required=True), limit=Int(required=True), offset=Int())
+    characterSeries = List(Series, id=ID(required=True), limit=Int(required=True), offset=Int())
+    characterStories = List(Storie, id=ID(required=True), limit=Int(required=True), offset=Int())
+
+    comics = List(Comics, limit=Int(required=True), offset=Int())
+    searchComics = Field(Comics, id=ID(required=True), resolver=resolve_comics.search_comics)
+    comicsCharacters = List(Character, id=ID(required=True), limit=Int(required=True), offset=Int())
+    comicsCreators = List(Creator, id=ID(required=True), limit=Int(required=True), offset=Int())
+    comicsEvents = List(Event, id=ID(required=True), limit=Int(required=True), offset=Int())
+    comicsStories = List(Storie, id=ID(required=True), limit=Int(required=True), offset=Int())
+
+    creators = List(Creator, limit=Int(required=True), offset=Int())
+    searchCreator = Field(Creator, id=ID(required=True), resolver=resolve_creator.search_creator)
+    creatorComics = List(Comics, id=ID(required=True), limit=Int(required=True), offset=Int())
+    creatorEvents = List(Event, id=ID(required=True), limit=Int(required=True), offset=Int())
+    creatorSeries = List(Series, id=ID(required=True), limit=Int(required=True), offset=Int())
+    creatorStories = List(Storie, id=ID(required=True), limit=Int(required=True), offset=Int())
+
+    events = List(Event, limit=Int(required=True), offset=Int())
+    searchEvent = Field(Event, id=ID(required=True), resolver=resolve_event.search_event)
+    eventCharacters = List(Character, id=ID(required=True), limit=Int(required=True), offset=Int())
+    eventComics = List(Comics, id=ID(required=True), limit=Int(required=True), offset=Int())
+    eventCreators = List(Creator, id=ID(required=True), limit=Int(required=True), offset=Int())
+    eventSeries = List(Series, id=ID(required=True), limit=Int(required=True), offset=Int())
+    eventStories = List(Storie, id=ID(required=True), limit=Int(required=True), offset=Int())
+
+    series = List(Series, limit=Int(required=True), offset=Int())
+    searchSerie = Field(Series, id=ID(required=True), resolver=resolve_series.search_serie)
+    serieCharacters = List(Character, id=ID(required=True), limit=Int(required=True), offset=Int())
+    serieComics = List(Comics, id=ID(required=True), limit=Int(required=True), offset=Int())
+    serieCreators = List(Creator, id=ID(required=True), limit=Int(required=True), offset=Int())
+    serieEvents = List(Event, id=ID(required=True), limit=Int(required=True), offset=Int())
+    serieStories = List(Storie, id=ID(required=True), limit=Int(required=True), offset=Int())
+    
+    stories = List(Storie, limit=Int(required=True), offset=Int())
+    searchStorie = Field(Storie, id=ID(required=True), resolver=resolve_storie.search_storie)
+    storieCharacters = List(Character, id=ID(required=True), limit=Int(required=True), offset=Int())
+    storieComics = List(Comics, id=ID(required=True), limit=Int(required=True), offset=Int())
+    storieCreators = List(Creator, id=ID(required=True), limit=Int(required=True), offset=Int())
+    storieEvents = List(Event, id=ID(required=True), limit=Int(required=True), offset=Int())
+    storieSeries = List(Series, id=ID(required=True), limit=Int(required=True), offset=Int())
 
     def resolve_characters(root, info, limit=None, offset=None):
-        return list_characters(limit, offset)
+        return resolve_character.list_characters(limit, offset)
 
-    def resolve_searchCharacter(root, info, id):
-        return search_character(id)
+    def resolve_characterComics(root, info, id, limit, offset=None):
+        return resolve_character.find_character_comics(id, limit, offset)    
+
+    def resolve_characterEvents(root, info, id, limit, offset=None):
+        return resolve_character.find_character_events(id, limit, offset)
+
+    def resolve_characterSeries(root, info, id, limit, offset=None):
+        return resolve_character.find_character_series(id, limit, offset)
+
+    def resolve_characterStories(root, info, id, limit, offset=None):
+        return resolve_character.find_character_stories(id, limit, offset)            
 
     def resolve_comics(root, info, limit=None, offset=None):
         return list_comics(limit, offset)
 
-    def resolve_searchComics(root, info, id):
-        return search_comics(id)
+    def resolve_comicsCharacters(root, info, id, limit, offset=None):
+        return resolve_comics.find_comics_characters(id, limit, offset)    
+
+    def resolve_comicsCreators(root, info, id, limit, offset=None):
+        return resolve_comics.find_comics_creators(id, limit, offset)
+
+    def resolve_comicsEvents(root, info, id, limit, offset=None):
+        return resolve_comics.find_comics_events(id, limit, offset)
+
+    def resolve_comicsStories(root, info, id, limit, offset=None):
+        return resolve_comics.find_comics_stories(id, limit, offset)
 
     def resolve_creators(root, info, limit=None, offset=None):
-        return list_creators(limit, offset)
+        return resolve_creator.list_creators(limit, offset)
 
-    def resolve_searchCreator(root, info, id):
-        return search_creator(id)
+    def resolve_creatorComics(root, info, id, limit, offset=None):
+        return resolve_creator.find_creator_comics(id, limit, offset)
+
+    def resolve_creatorEvents(root, info, id, limit, offset=None):
+        return resolve_creator.find_creator_events(id, limit, offset)
+
+    def resolve_creatorSeries(root, info, id, limit, offset=None):
+        return resolve_creator.find_creator_series(id, limit, offset)
+
+    def resolve_creatorStories(root, info, id, limit, offset=None):
+        return resolve_creator.find_creator_stories(id, limit, offset)
 
     def resolve_events(root, info, limit=None, offset=None):
-        return list_events(limit, offset)
+        return resolve_event.list_events(limit, offset)
 
-    def resolve_searchEvent(root, info, id):
-        return search_event(id)
+    def resolve_eventCharacters(root, info, id, limit, offset=None):
+        return resolve_event.find_event_characters(id, limit, offset)
+
+    def resolve_eventComics(root, info, id, limit, offset=None):
+        return resolve_event.find_event_comics(id, limit, offset)
+
+    def resolve_eventCreators(root, info, id, limit, offset=None):
+        return resolve_event.find_event_creators(id, limit, offset)
+
+    def resolve_eventSeries(root, info, id, limit, offset=None):
+        return resolve_event.find_event_series(id, limit, offset)
+
+    def resolve_eventStories(root, info, id, limit, offset=None):
+        return resolve_event.find_event_stories(id, limit, offset)
 
     def resolve_series(root, info, limit=None, offset=None):
-        return list_series(limit, offset)
+        return resolve_series.list_series(limit, offset)
 
-    def resolve_searchSerie(root, info, id):
-        return search_serie(id)
+    def resolve_serieCharacters(root, info, id, limit, offset=None):
+        return resolve_series.find_serie_characters(id, limit, offset)
+
+    def resolve_serieComics(root, info, id, limit, offset=None):
+        return resolve_series.find_serie_comics(id, limit, offset)
+
+    def resolve_serieCreators(root, info, id, limit, offset=None):
+        return resolve_series.find_serie_creators(id, limit, offset)
+
+    def resolve_serieEvents(root, info, id, limit, offset=None):
+        return resolve_series.find_serie_events(id, limit, offset)
+
+    def resolve_serieStories(root, info, id, limit, offset=None):
+        return resolve_series.find_serie_stories(id, limit, offset)
 
     def resolve_stories(root, info, limit=None, offset=None):
-        return list_stories(limit, offset)
+        return resolve_storie.list_stories(limit, offset)
 
-    def resolve_searchStorie(root, info, id):
-        return search_storie(id)                     
+    def resolve_storieCharacters(root, info, id, limit, offset=None):
+        return resolve_storie.find_storie_characters(id, limit, offset)
+
+    def resolve_storieComics(root, info, id, limit, offset=None):
+        return resolve_storie.find_storie_comics(id, limit, offset)
+
+    def resolve_storieCreators(root, info, id, limit, offset=None):
+        return resolve_storie.find_storie_creators(id, limit, offset)
+
+    def resolve_storieEvents(root, info, id, limit, offset=None):
+        return resolve_storie.find_storie_events(id, limit, offset)
+
+    def resolve_storieSeries(root, info, id, limit, offset=None):
+        return resolve_storie.find_storie_series(id, limit, offset)    
 
 schema = Schema(query=Query)
