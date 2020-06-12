0d928a5a0a6ce55de6173b93bb8e5cdb3aae0530
Álvaro Oliveira <alvarogab6@gmail.com>
2020-06-12 12:38:13 -0300

    fix

--- app/types/storie.py
+++
@@ -1,8 +0,0 @@
-import graphene
-
-class Storie(graphene.ObjectType):
-    id = graphene.ID(required=True)
-    title = graphene.String(required=True)
-    description = graphene.String()
-    type = graphene.String()
-    
